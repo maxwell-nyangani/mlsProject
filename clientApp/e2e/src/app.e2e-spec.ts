4cfb0010a7ce39a7de2b17b0b11f5020f6b4342d
m_a_x_w_e_l_l <max.nyanx@gmail.com>
2019-02-03 21:11:08 +0100

    first route

--- clientApp/e2e/src/app.e2e-spec.ts
+++ clientApp/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to testApp!');
+    expect(page.getTitleText()).toEqual('Welcome to clientApp!');
   });
 
   afterEach(async () => {
--- clientApp/src/app/app.component.spec.ts
+++ clientApp/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'testApp'`, () => {
+  it(`should have as title 'clientApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('testApp');
+    expect(app.title).toEqual('clientApp');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to testApp!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to clientApp!');
   });
 });
