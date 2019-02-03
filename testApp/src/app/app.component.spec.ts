2a7a847fe5eb700e805bbd82f78770c055ca763b
m_a_x_w_e_l_l <max.nyanx@gmail.com>
2019-02-03 19:17:37 +0100

    changed to css

--- testApp/src/app/app.component.spec.ts
+++ testApp/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'mlsTest'`, () => {
+  it(`should have as title 'testApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('mlsTest');
+    expect(app.title).toEqual('testApp');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to mlsTest!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to testApp!');
   });
 });
